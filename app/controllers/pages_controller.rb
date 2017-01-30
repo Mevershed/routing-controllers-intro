183f304f696a3f25d9548ace5764f44f5779b48d
Mevershed <matthew@evershed.ca>
2017-01-30 17:38:14 -0500

    created flash notfications

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
   def about
     @header = "What would you like to know?"
   end
@@ -6,10 +7,22 @@ class PagesController < ApplicationController
     @header = "Welcome to the page!"
   end
   def contest
-    @header = "This contest is for nothing!"
+    # @header = "This contest is for nothing!"
+    flash[:notice] = "Sorry, the contest has ended"
+    redirect_to "/welcome"
   end
   def kitten
+    # set_kitten_url
     # @header = "KITTENS!"
+    # requested_size = params[:size]
+    # @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+  def kittens
+    # set_kitten_url
+    # requested_size = params[:size]
+    # @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+  def set_kitten_url
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
