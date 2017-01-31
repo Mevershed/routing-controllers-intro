6e588ea51b48e096dbb0a08b8c991b94f0191ae4
Mevershed <matthew@evershed.ca>
2017-01-31 10:11:24 -0500

    submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
+  root 'pages#welcome'
   get '/welcome' => 'pages#welcome'
-  get '/' => 'pages#welcome'
   get '/about' => 'pages#about'
   get '/contest' => 'pages#contest'
-  get '/kitten/:size' => 'pages#kitten'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
   get '/kittens/:size' => 'pages#kittens'
   get '/secrets/:magic_word' => 'pages#secrets'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
